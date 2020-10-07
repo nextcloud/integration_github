c9783b93e8d4d510de8146502143629c0f5dc274
Julien Veyssier <eneiluj@posteo.net>
2020-10-07 19:06:22 +0200

    bump vue loader
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- webpack.js
+++ webpack.js
@@ -4,15 +4,15 @@ const webpack = require('webpack')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
 if (webpackConfig.entry && webpackConfig.entry.main) {
-    delete webpackConfig.entry.main
+	delete webpackConfig.entry.main
 }
 
 const config = {
-    entry: {
+	entry: {
 		personalSettings: path.join(__dirname, 'src', 'personalSettings.js'),
 		adminSettings: path.join(__dirname, 'src', 'adminSettings.js'),
 		dashboard: path.join(__dirname, 'src', 'dashboard.js'),
-    },
+	},
 }
 
 module.exports = merge(config, webpackConfig)
