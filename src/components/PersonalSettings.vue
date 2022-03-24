82fc7f335c927ff5b14970193686e7de69eb7e40
Tobias Speicher <rootcommander@gmail.com>
2022-03-24 16:58:22 +0100

    Replace deprecated String.prototype.substr()
    .substr() is deprecated so we replace it with .slice() which works similarily but isn't deprecated
    
    Signed-off-by: Tobias Speicher <rootcommander@gmail.com>

--- src/components/PersonalSettings.vue
+++ src/components/PersonalSettings.vue
@@ -140,7 +140,7 @@ export default {
 	},
 
 	mounted() {
-		const paramString = window.location.search.substr(1)
+		const paramString = window.location.search.slice(1)
 		// eslint-disable-next-line
 		const urlParams = new URLSearchParams(paramString)
 		const ghToken = urlParams.get('githubToken')
