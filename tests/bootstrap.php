bcc3e991e92c3aef6d81d2914784eae887c2cfaf
Julien Veyssier <julien-nc@posteo.net>
2025-09-30 14:48:48 +0200

    attempt to fix tests
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,5 +5,5 @@ use OCP\App\IAppManager;
 
 require_once __DIR__ . '/../../../tests/bootstrap.php';
 
-\OC::$server->get(IAppManager::class)->loadApp(Application::APP_ID);
+\OCP\Server::get(IAppManager::class)->loadApp(Application::APP_ID);
 OC_Hook::clear();
